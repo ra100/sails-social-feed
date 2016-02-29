d1577dc1c9a53437d3c55e169c647013f965b5c7
Ra100 <ra100x@gmail.com>
2016-02-29 14:56:28 +0100

    new policies, dummy

---
+++ api/policies/isEditor.js
@@ -0,0 +1,13 @@
+/**
+ * isEditor
+ *
+ * @module      :: Policy
+ * @description :: TODO: You might write a short summary of how this policy works and what it represents here.
+ * @help        :: http://sailsjs.org/#!/documentation/concepts/Policies
+ */
+module.exports = function(req, res, next) {
+  var uid = req.user.id;
+  console.log(uid);
+  return next();
+
+};
