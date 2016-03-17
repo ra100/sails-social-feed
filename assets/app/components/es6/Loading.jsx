38e46e831defc54796b6eacce64cef7160d86010
Ra100 <ra100x@gmail.com>
2016-03-17 12:18:43 +0100

    Loading screen

---
+++ assets/app/components/es6/Loading.jsx
@@ -0,0 +1,29 @@
+import {Component, PropTypes} from 'react';
+import {Panel, ProgressBar} from 'react-bootstrap';
+import {FormattedMessage, defineMessages, injectIntl} from 'react-intl';
+
+const messages = defineMessages({
+  loading: {
+    id: 'message.loading.title',
+    description: 'loading',
+    defaultMessage: 'Loading'
+  }
+});
+
+class Loading extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    const {formatMessage} = this.props.intl;
+    return (
+      <Panel header={formatMessage(messages.loading)}>
+        <ProgressBar active now={100} striped/>
+      </Panel>
+    );
+  }
+}
+
+export default injectIntl(Loading);
