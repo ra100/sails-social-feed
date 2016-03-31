d8c5f60d237cb1fd015da2a73a3fea6f9275f1e5
Ra100 <ra100x@gmail.com>
2016-03-31 17:05:23 +0200

    message lifecycle hooks

--- api/models/Message.js
+++ api/models/Message.js
@@ -9,16 +9,23 @@ module.exports = {
 
   attributes: {
     stream: {
-      model: 'Stream'
+      model: 'Stream',
+      required: true
     },
     feedType: {
       type: 'string'
     },
     feed: {
-      model: 'Feed'
+      model: 'Feed',
+      required: true
     },
     message: {
-      type: 'text'
+      type: 'text',
+      required: true
+    },
+    published: {
+      type: 'boolean',
+      defaultsTo: false
     },
     created: {
       type: 'datetime'
@@ -47,6 +54,60 @@ module.exports = {
       type: 'boolean',
       defaultsTo: false
     }
+  },
+
+  /**
+   * Automatically fill some values
+   */
+  beforeValidate: function (values, next) {
+    if (!values.created) {
+      values.created = new Date();
+    }
+    if (values.feed) {
+      Feed.findOne(values.feed).populate('stream').then(function (feed) {
+        values.stream = feed.stream.id;
+        values.feedType = feed.type;
+        values.published = feed.stream.display;
+        next();
+      }).catch(function (err) {
+        next(err);
+      });
+    } else {
+      next();
+    }
+  },
+
+  /**
+   * Send publishAdd message to sockets, when new message is published
+   */
+  afterCreate: function (values, next) {
+    if (values.published) {
+      delete values._csrf;
+      Stream.publishAdd(values.stream, 'messages', values);
+    }
+    next();
+  },
+
+  /**
+   * Send remove message to sockets if message has been upublished
+   */
+  beforeUpdate: function (values, next) {
+    if (values.published !== undefined && !values.published) {
+      delete values._csrf;
+      Stream.publishRemove(values.stream, 'messages', values.id);
+    }
+    next();
+  },
+
+  /**
+   * Send publishAdd message to sockets,
+   * when message has been changed and is publushed
+   */
+  afterUpdate: function (values, next) {
+    if (values.published) {
+      delete values._csrf;
+      Stream.publishAdd(values.stream, 'messages', values);
+    }
+    next();
   }
 };
-
