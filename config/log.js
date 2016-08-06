16f0797b2644992213964f604c5885fcde1d7add
ra100 <ra100x@gmail.com>
2016-08-06 16:46:39 +0200

    Add logging to file

--- config/log.js
+++ config/log.js
@@ -10,20 +10,39 @@
  * http://sailsjs.org/#!/documentation/concepts/Logging
  */
 
+var winston = require('winston');
+var customLogger = new winston.Logger();
+
+// A console transport logging debug and above.
+customLogger.add(winston.transports.Console, {
+  level: 'debug',
+  colorize: true
+});
+
+// A file based transport logging only errors formatted as json.
+customLogger.add(winston.transports.File, {
+  level: 'debug',
+  filename: 'app.log',
+  json: true
+});
+
 module.exports.log = {
 
   /***************************************************************************
-  *                                                                          *
-  * Valid `level` configs: i.e. the minimum log level to capture with        *
-  * sails.log.*()                                                            *
-  *                                                                          *
-  * The order of precedence for log levels from lowest to highest is:        *
-  * silly, verbose, info, debug, warn, error                                 *
-  *                                                                          *
-  * You may also set the level to "silent" to suppress all logs.             *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Valid `level` configs: i.e. the minimum log level to capture with        *
+   * sails.log.*()                                                            *
+   *                                                                          *
+   * The order of precedence for log levels from lowest to highest is:        *
+   * silly, verbose, info, debug, warn, error                                 *
+   *                                                                          *
+   * You may also set the level to "silent" to suppress all logs.             *
+   *                                                                          *
+   ***************************************************************************/
 
   // level: 'verbose'
+  custom: customLogger,
+  // Disable captain's log so it doesn't prefix or stringify our meta data.
+  inspect: false
 
 };
