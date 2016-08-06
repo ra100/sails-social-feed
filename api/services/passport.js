34a6ab3b37d3382b5b9bb2ece4dda2e11dc79df9
ra100 <ra100x@gmail.com>
2016-08-06 15:28:05 +0200

    Add facebook login test

--- api/services/passport.js
+++ api/services/passport.js
@@ -98,6 +98,15 @@ passport.connect = function (req, query, profile, next) {
     };
   }
 
+  if (profile.provider == 'facebook') {
+    user.displayname = profile.displayName;
+    user.picture = profile.picture.data.url;
+    user.meta = {
+      name: profile.username,
+      displayname: profile.displayName
+    };
+  }
+
   // If neither an email or a username was available in the profile, we don't
   // have a way of identifying the user in the future. Throw an error and let
   // whoever's next in the line take care of it.
