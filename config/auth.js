cc3c64fdb7d445c8bc87a12fe0d12e31177d2f0a
ra100 <ra100x@gmail.com>
2016-08-06 16:46:08 +0200

    Add services to auth conf

--- config/auth.js
+++ config/auth.js
@@ -3,5 +3,11 @@ module.exports.auth = {
   twitter_consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   facebook_app_id: process.env.FACEBOOK_APP_ID,
   facebook_app_secret: process.env.FACEBOOK_APP_SECRET,
-  facebook_api_version: process.env.FACEBOOK_API_VERSION
+  facebook_api_version: process.env.FACEBOOK_API_VERSION,
+  google_client_if: process.env.GOOGLE_CLIENT_ID,
+  google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
+  instagram_client_id: process.env.INSTAGRAM_CLIENT_ID,
+  instagram_client_secret: process.env.INSTAGRAM_CLIENT_SECRET,
+  soundcloud_client_id: process.env.SOUNDCLOUD_CLIENT_ID,
+  soundcloud_client_secret: process.env.SOUNDCLOUD_CLIENT_SECRET,
 };
